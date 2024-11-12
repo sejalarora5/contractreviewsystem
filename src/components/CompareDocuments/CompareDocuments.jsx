8368fe3b973d73da32d31821ef1c76ff223bde68
Netsmartz <netsmartz@administrators-Mac-mini.local>
2024-11-12 10:35:22 +0530

    home page ui is almost done

--- src/components/CompareDocuments/CompareDocuments.jsx
+++ src/components/CompareDocuments/CompareDocuments.jsx
@@ -13,7 +13,6 @@ const CompareDocuments = () => {
   const [redlinedDoc, setRedlinedDoc] = useState('')
   const [standardDoc, setStandardDoc] = useState('')
 
-
   const handleCompare = async () => {
     setLoading(true);
     setError(null);
@@ -21,7 +20,6 @@ const CompareDocuments = () => {
 
     try {
       const url = 'https://9669-112-196-16-34.ngrok-free.app/comparison/compare-documents';
-      console.log('token in compare', token);
       const response = await axios.get(url, {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -29,17 +27,11 @@ const CompareDocuments = () => {
           'Accept': 'application/json',
         },
       });
-      console.log("comparison data:", response.data);
-      console.log("response status:", response.status);
-      console.log("response headers:", response.headers);
-      console.log("responseeeeeee:", response.data.standard_file_name);
       setComparisonData(response.data.comparison_result.changes);
-      setStandardDoc(response.data.standard_file_name)
-      setRedlinedDoc(response.data.redlined_file_name)
-
+      setStandardDoc(response.data.standard_file_name);
+      setRedlinedDoc(response.data.redlined_file_name);
     } catch (err) {
       setError('Failed to fetch comparison data.');
-      console.error("Error fetching comparison data:", err);
     } finally {
       setLoading(false);
     }
@@ -58,7 +50,7 @@ const CompareDocuments = () => {
   };
 
   return (
-    <div className="col-span-2 bg-white p-4 shadow rounded-lg flex flex-col items-center ">
+    <div className="col-span-2 bg-white p-4 shadow rounded-lg flex flex-col items-center relative">
       {loading && (
         <div className="flex justify-center items-center w-full h-full absolute top-0 left-0 bg-opacity-50 bg-gray-800 z-50">
           <div className="w-16 h-16 border-4 border-t-4 border-gray-300 border-t-[#f58220] rounded-full animate-spin"></div>
@@ -74,7 +66,7 @@ const CompareDocuments = () => {
       )}
 
       {!loading && !comparisonData && (
-        <div className='w-full min-h-screen flex flex-col items-center justify-center'>
+        <div className='w-full min-h-full flex flex-col items-center justify-center'>
           <h2 className="font-semibold text-xl mb-4 text-center">Compare your Documents</h2>
           <button
             onClick={handleCompare}
@@ -84,137 +76,81 @@ const CompareDocuments = () => {
           </button>
         </div>
       )}
+      
+      {!loading && comparisonData && (
+        <div className="w-full h-[650px] space-y-6 text-left flex flex-col overflow-auto">
+          <h3 className="font-semibold text-2xl text-gray-800 tracking-wide text-center">Comparison Results</h3>
 
-      <div className="w-full h-5/6 space-y-6 text-left flex flex-col">
-        <h3 className="font-semibold text-2xl text-gray-800 tracking-wide text-center">Comparison Results</h3>
-
-        <div className="bg-white p-6 rounded-lg shadow-xl border border-gray-200 flex-grow overflow-y-auto">
-          <div className="flex items-center justify-around space-x-8 mb-6">
-            <div className="text-center flex">
-              <img
-                src={document}
-                alt="Standard Document"
-                className="w-20 h-20 object-cover rounded-lg shadow-md"
-              />
-              <div>
-                <p className="text-s ml-2 font-medium mt-2 text-left text-gray-800">Standard Document</p>
-                <p className="text-sm ml-2 text-[#f58220] text-left line-clamp-2 max-w-xs sm:max-w-[10rem] md:max-w-[12rem] lg:max-w-[14rem]">
-                  {standardDoc}
-                </p>
+          <div className="bg-white pt-3 pl-2 pr-2 rounded-lg shadow-xl border border-gray-200 flex-grow overflow-hidden">
+            <div className="flex items-center pt-2 justify-around space-x-8 mb-6">
+              <div className="text-center flex">
+                <img
+                  src={document}
+                  alt="Standard Document"
+                  className="w-20 h-20 object-cover rounded-lg shadow-md"
+                />
+                <div>
+                  <p className="text-s ml-2 font-medium mt-2 text-left text-gray-800">Standard Document</p>
+                  <p className="text-sm ml-2 text-[#f58220] text-left line-clamp-2 max-w-xs sm:max-w-[10rem] md:max-w-[12rem] lg:max-w-[14rem]">
+                    {standardDoc}
+                  </p>
+                </div>
+              </div>
+              <div className="text-center flex">
+                <img
+                  src={document}
+                  alt="Redlined Document"
+                  className="w-20 h-20 object-cover rounded-lg shadow-md"
+                />
+                <div>
+                  <p className="text-s ml-2 font-medium mt-2 text-left text-gray-800">Redlined Document</p>
+                  <p className="text-sm ml-2 text-[#f58220] text-left line-clamp-2 max-w-xs sm:max-w-[10rem] md:max-w-[12rem] lg:max-w-[14rem]">
+                    {redlinedDoc}
+                  </p>
+                </div>
               </div>
             </div>
-            <div className="text-center flex">
-              <img
-                src={document}
-                alt="Redlined Document"
-                className="w-20 h-20 object-cover rounded-lg shadow-md"
-              />
-              <div>
-                <p className="text-s ml-2 font-medium mt-2 text-left text-gray-800">Redlined Document</p>
-                <p className="text-sm ml-2 text-[#f58220] text-left line-clamp-2 max-w-xs sm:max-w-[10rem] md:max-w-[12rem] lg:max-w-[14rem]">
-                  {redlinedDoc}
-                </p>
-              </div>
-            </div>
-          </div>
 
-          <div className="mt-2 space-y-3">
-            <p className="font-semibold text-lg text-[#f58220] pb-2">
-              Section: <span className="font-medium text-gray-700">{comparisonData[currentPage].section}</span>
-            </p>
+            <div className="mt-2 space-y-3 overflow-y-auto max-h-[350px]">
+              <p className="font-semibold text-lg text-[#f58220] pb-2">
+                Section: <span className="font-medium text-gray-700">{comparisonData[currentPage].section}</span>
+              </p>
 
-            <p className="text-gray-700 pb-2">
-              <span className="font-medium text-[#f58220]">Original Text:</span> {comparisonData[currentPage].original_text}
-            </p>
-            <p className="text-gray-700 pb-2">
-              <span className="font-medium text-[#f58220]">Revised Text:</span> {comparisonData[currentPage].revised_text}
-            </p>
-            <p className="text-gray-700 pb-2">
-              <span className="font-medium text-[#f58220]">Nature of Change:</span> {comparisonData[currentPage].nature_of_change}
-            </p>
-            <p className="text-gray-700 pb-2">
-              <span className="font-medium text-[#f58220]">Favorable:</span> {comparisonData[currentPage].favorable}
-            </p>
-          </div>
-        </div>
-
-        {/* Fixed button container */}
-        <div className="mt-4 flex justify-between space-x-4">
-          <button
-            onClick={prevPage}
-            disabled={currentPage === 0}
-            className="px-6 py-3 w-1/5 text-white bg-gray-600 rounded-lg hover:bg-gray-700 disabled:opacity-50 transition duration-200"
-          >
-            Previous
-          </button>
-          <button
-            onClick={nextPage}
-            disabled={currentPage === comparisonData.length - 1}
-            className="px-6 py-3 w-1/5 text-white bg-orange-500 rounded-lg hover:bg-orange-600 disabled:opacity-50 transition duration-200"
-          >
-            Next
-          </button>
-        </div>
-      </div>
-
-      {/* {!loading && comparisonData && ( */}
-      {/* <div className="w-full h-full mt-6 space-y-6 text-left">
-
-        <h3 className="font-semibold text-2xl text-gray-800 tracking-wide text-center">Comparison Results</h3>
-        <div className="bg-white p-6 rounded-lg shadow-xl border border-gray-200 h-fit justify-center item-center">
-          <div className="flex items-center justify-around mb-6 space-x-8">
-            <div className="text-center flex">
-              <img
-                src={document}
-                alt="Standard Document"
-                className="w-20 h-20 object-cover rounded-lg shadow-md"
-              />
-              <div>
-                <p className="text-s ml-2 font-medium mt-2 text-left text-gray-800">Standard Document</p>
-                <p className="text-sm ml-2 text-[#f58220] text-left line-clamp-2 max-w-xs sm:max-w-[10rem] md:max-w-[12rem] lg:max-w-[14rem]">
-                  redlinedDoc redlinedDoc redlinedDoc redlinedDoc
-                </p>
-              </div>
-            </div>
-            <div className="text-center flex">
-              <img
-                src={document}
-                alt="Redlined Document"
-                className="w-20 h-20 object-cover rounded-lg shadow-md"
-              />
-              <div>
-                <p className="text-s ml-2 font-medium mt-2 text-left text-gray-800">Redlined Document</p>
-                <p className="text-sm ml-2 text-[#f58220] text-left line-clamp-2 max-w-xs sm:max-w-[10rem] md:max-w-[12rem] lg:max-w-[14rem]">
-                  redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc
-                </p>
-              </div>
+              <p className="text-gray-700 pb-2">
+                <span className="font-medium text-[#f58220]">Original Text:</span> {comparisonData[currentPage].original_text}
+              </p>
+              <p className="text-gray-700 pb-2">
+                <span className="font-medium text-[#f58220]">Revised Text:</span> {comparisonData[currentPage].revised_text}
+              </p>
+              <p className="text-gray-700 pb-2">
+                <span className="font-medium text-[#f58220]">Nature of Change:</span> {comparisonData[currentPage].nature_of_change}
+              </p>
+              <p className="text-gray-700 pb-2">
+                <span className="font-medium text-[#f58220]">Favorable:</span> {comparisonData[currentPage].favorable}
+              </p>
             </div>
           </div>
-          <div className="mt-2 space-y-3">
-            <p className="font-semibold text-lg text-[#f58220] pb-2">Section: <span className="font-medium text-gray-700">Original Text:</span>
-            </p>
 
-            <p className="text-gray-700 pb-2">
-              <span className="font-medium text-[#f58220]">Original Text:</span> comparisonData[currentPage].original_text redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDocredlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc redlinedDoc
-            </p>
-            <p className="text-gray-700 pb-2">
-              <span className="font-medium text-[#f58220]">Revised Text:</span> comparisonData[currentPage].revised_text
-            </p>
-            <p className="text-gray-700 pb-2">
-              <span className="font-medium text-[#f58220]">Nature of Change:</span> blah cblajkaa
-            </p>
-            <p className="text-gray-600 pb-2">
-              <span className="font-medium text-[#f58220]">Favorable:</span> blah blah
-            </p>
+          {/* Button container with fixed position */}
+          <div className="mt-4 flex justify-between space-x-4">
+            <button
+              onClick={prevPage}
+              disabled={currentPage === 0}
+              className="px-6 py-3 w-1/5 text-white bg-gray-600 rounded-lg hover:bg-gray-700 disabled:opacity-50 transition duration-200"
+            >
+              Previous
+            </button>
+            <button
+              onClick={nextPage}
+              disabled={currentPage === comparisonData.length - 1}
+              className="px-6 py-3 w-1/5 text-white bg-orange-500 rounded-lg hover:bg-orange-600 disabled:opacity-50 transition duration-200"
+            >
+              Next
+            </button>
           </div>
         </div>
-
-
-
-      </div> */}
-      {/* )}*/}
+      )}
     </div>
-
   );
 };
 
--- src/navigation/Home/HomePage.jsx
+++ src/navigation/Home/HomePage.jsx
@@ -1,215 +1,10 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import FileUpload from '../../components/FileUpload/FileUpload';
-import { AiOutlineDelete } from 'react-icons/ai';
 import { RiDeleteBin5Fill } from "react-icons/ri";
 import { useSelector } from "react-redux";
 const baseUrl = import.meta.env.VITE_BASE_URL;
-
 import axios from 'axios';
-import DocumentDetails from '../../components/DocumentDetaills/DocumentDetails';
 import CompareDocuments from '../../components/CompareDocuments/CompareDocuments';
-import MyComponent from '../../components/MyComponent';
-// const HomePage = () => {
-
-//   const [standardFile, setStandardFile] = useState(null);
-//   const [revisedFile, setRevisedFile] = useState(null);
-//   const [loading, setLoading] = useState(false);
-//   const [progress, setProgress] = useState(0);
-//   const [error, setError] = useState('');
-//   const token = useSelector((state) => state.auth.token);
-//     console.log("tokenn bearer", token)
-
-//   // Handle file selection for standard and revised files
-//   const handleStandardFileSelect = (file) => setStandardFile(file);
-//   const handleRevisedFileSelect = (file) => setRevisedFile(file);
-
-//   // Handle file upload when "Process Documents" button is clicked
-//   const handleUpload = async () => {
-//     if (!standardFile || !revisedFile) {
-//       setError('Please upload both files');
-//       alert('Please upload both files')
-//       return;
-//     }
-
-//     setLoading(true);
-//     const formData = new FormData();
-//     formData.append('standard_file', standardFile);
-//     formData.append('revised_file', revisedFile);
-
-//     try {
-//       const response = await axios.post(`${baseUrl}upload-documents`, formData, {
-//         headers: {
-//           'Content-Type': 'multipart/form-data',
-//           'Authorization': `Bearer ${token}`,
-//         },
-//         onUploadProgress: (event) => {
-//           const progress = Math.round((event.loaded * 100) / event.total);
-//           setProgress(progress);
-//         },
-//       });
-
-//       console.log("Upload response:", response.data);
-//       setLoading(false);
-//       setProgress(100); // Set to 100% on completion
-//     } catch (error) {
-//       setError('Failed to upload files');
-//       console.error("Upload error:", error);
-//       setLoading(false);
-//       setProgress(0);
-//     }
-//   };
-//   const handleProcessDocuments = async () => {
-//     if (!standardFile || !revisedFile) {
-//       alert("Please upload both files before processing.");
-//       return;
-//     }
-
-//     const formData = new FormData();
-//     formData.append("standard_file", standardFile);
-//     formData.append("revised_file", revisedFile);
-
-//     try {
-//       const response = await axios.post(`${baseUrl}upload-documents`, formData, {
-//         headers: {
-//           "Content-Type": "multipart/form-data",
-//           Authorization: `Bearer ${token}`,
-//         },
-//       });
-//       console.log("Documents processed:", response.data);
-//       alert("Documents processed successfully!");
-//     } catch (error) {
-//       console.error("Error uploading documents:", error);
-//       alert("Failed to process documents.");
-//     }
-//   };
-
-//   const handleDeleteDocuments = async () => {
-//     try {
-//       // Call the delete API to remove uploaded documents
-//       const response = await fetch('/api/delete-documents', {
-//         method: 'DELETE',
-//       });
-//       if (response.ok) {
-//         console.log("Documents deleted successfully");
-//         // Optionally clear state or show a success message here
-//       } else {
-//         console.error("Failed to delete documents");
-//       }
-//     } catch (error) {
-//       console.error("Error deleting documents:", error);
-//     }
-//   };
-//   return (
-//     <div className="min-h-screen bg-white-50 p-4">
-//       {/* NavBar */}
-//       <nav className="bg-black text-white flex items-center justify-between p-4 mb-4">
-//         <div className="flex items-center space-x-4">
-//           <button className="btn btn-ghost text-lg text-white normal-case">Compare</button>
-//           <button className="btn btn-ghost text-lg text-gray-400 normal-case">Validation</button>
-//         </div>
-//         <div className="dropdown dropdown-end">
-//           <button className="btn btn-ghost btn-circle">
-//             <svg
-//               xmlns="http://www.w3.org/2000/svg"
-//               className="h-5 w-5"
-//               fill="none"
-//               viewBox="0 0 24 24"
-//               stroke="currentColor"
-//             >
-//               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
-//             </svg>
-//           </button>
-//           <ul className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-//             <li><a href="#">Profile</a></li>
-//             <li><a href="#">Settings</a></li>
-//             <li><a href="#">Logout</a></li>
-//           </ul>
-//         </div>
-//       </nav>
-
-//       {/* Main Content */}
-//       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-//         {/* Left Section: File Upload */}
-//         <div className="col-span-1 space-y-2">
-//           <div className="flex flex-col items-center space-y-2">
-//           </div>
-//           <FileUpload labelText="Upload standard file" onFileSelect={handleStandardFileSelect}  />
-//           <FileUpload labelText="Upload redlined file" onFileSelect={handleRevisedFileSelect} />
-//              {/* Show progress */}
-//       {loading && (
-//         <div className="mt-4">
-//           <div>Uploading: {progress}%</div>
-//           <div className="bg-gray-200 rounded-full">
-//             <div
-//               className="bg-[#F39200] text-xs leading-none py-1 text-center text-white rounded-full"
-//               style={{ width: `${progress}%` }}
-//             />
-//           </div>
-//         </div>
-//       )}
-
-//       {/* Success/Error Messages */}
-//       {!loading && !error && (standardFile || revisedFile) && progress === 100 && (
-//         <div className="mt-4 text-[#F39200] text-sm">Files uploaded successfully!</div>
-//       )}
-
-//       {error && <div className="mt-4 text-red-500">{error}</div>}
-//           <button onClick={handleUpload} className="btn btn-block mt-4" >Process Documents</button>
-//           <button
-//             className="flex items-center btn-block justify-center space-x-2 text-red-500 mt-2"
-//             onClick={handleDeleteDocuments}
-//           >
-//             <RiDeleteBin5Fill className="w-6 h-8" />
-//             {/* <span className='text-center text-black'>Delete Documents</span> */}
-//           </button>
-//         </div>
-
-//         {/* Center Section: Document Details */}
-
-//         <div class="flex justify-center items-center">
-//     <button 
-//         type="button" 
-//         class="text-white bg-gradient-to-r from-[#FF7E5F] via-[#FEB47B] to-[#FF7E5F] hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-orange-200 dark:focus:ring-orange-800 font-medium rounded-full text-lg px-12 py-6 text-center shadow-lg transform transition duration-300 hover:scale-105 w-full md:w-auto md:px-14 lg:px-16"
-//     >
-//         Attractive Gradient Button
-//     </button>
-// </div>
-
-
-//        {/* <DocumentDetails/> */}
-
-//         {/* Right Section: History */}
-//         <div className="col-span-1 bg-white p-4 shadow rounded-lg">
-//           <h3 className="font-bold mb-4">History</h3>
-//           <ul className="space-y-2">
-//             {["2023-10-01", "2023-09-28", "2023-09-25", "2023-09-20", "2023-09-15"].map((date, index) => (
-//               <li key={index} className="flex items-center space-x-2">
-//                 <div className="w-8 h-8 bg-gray-200 rounded-full"></div>
-//                 <span>Comparison {index + 1}</span>
-//                 <span className="text-gray-500 text-sm">{date}</span>
-//               </li>
-//             ))}
-//           </ul>
-//         </div>
-//       </div>
-//     </div>
-
-//   )
-// }
-
-// export default HomePage
-
-
-
-// import React, { useState } from 'react';
-// import FileUpload from '../../components/FileUpload/FileUpload';
-// import { AiOutlineDelete } from 'react-icons/ai';
-// import { RiDeleteBin5Fill } from "react-icons/ri";
-// import { useSelector } from "react-redux";
-// import axios from 'axios';
-// import DocumentDetails from '../../components/DocumentDetails/DocumentDetails';
-
-// const baseUrl = import.meta.env.VITE_BASE_URL;
 
 const HomePage = () => {
   const [standardFile, setStandardFile] = useState(null);
@@ -218,7 +13,6 @@ const HomePage = () => {
   const [progress, setProgress] = useState(0);
   const [error, setError] = useState('');
   const token = useSelector((state) => state.auth.token);
-  // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJha3NoYXkiLCJleHAiOjE3MzEzMzk3NjZ9.amUNsznP7I2exgfYd7gTHn2kT3fAM8s4GmWr9OLeunE'
 
   const handleStandardFileSelect = (file) => setStandardFile(file);
   const handleRevisedFileSelect = (file) => setRevisedFile(file);
@@ -260,7 +54,7 @@ const HomePage = () => {
     try {
       const response = await fetch(`${baseUrl}delete-documents`, {
         method: 'DELETE',
-        headers:  {
+        headers: {
           'Content-Type': 'multipart/form-data',
           'Authorization': `Bearer ${token}`,
         },
@@ -311,7 +105,7 @@ const HomePage = () => {
               </div>
             </div>
           )}
-          
+
           {!loading && !error && (standardFile || revisedFile) && progress === 100 && (
             <div className="mt-4 text-[#F39200] text-sm">Files uploaded successfully!</div>
           )}
@@ -321,20 +115,11 @@ const HomePage = () => {
           <button className="flex items-center btn-block justify-center space-x-2 text-red-500 mt-2" onClick={handleDeleteDocuments}>
             <RiDeleteBin5Fill className="w-6 h-8" />
           </button>
-          {/* <MyComponent/> */}
+
         </div>
 
-        {/* Center Section: Document Details and Compare Button - 50% Width */}
-        {/* <div className="col-span-2 bg-white p-4 shadow rounded-lg flex flex-col items-center justify-center"> */}
-          {/* <h2 className="font-semibold text-xl mb-4 text-center">Compare your Documents </h2>
-          <button 
-            className="text-white bg-gradient-to-r from-[#FF7E5F] via-[#FEB47B] to-[#FF7E5F] hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-orange-200 dark:focus:ring-orange-800 font-medium rounded-full text-lg py-3 w-2/5 text-center shadow-lg transform transition duration-300 hover:scale-105"
-          >
-            Compare
-          </button> */}
-          <CompareDocuments/>
-          {/* <DocumentDetails /> */}
-        {/* </div> */}
+
+        <CompareDocuments />
 
         {/* Right Section: History - 25% Width */}
         <div className="col-span-1 bg-white p-4 shadow rounded-lg">
